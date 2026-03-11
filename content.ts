1b389b7a47303564b2907d2e1f4126abba8110a4
Natsuneco <135076690+Natsuneco@users.noreply.github.com>
2026-03-11 14:28:41 +0900

    エピソードの切り替わりに対応

--- content.ts
+++ content.ts
@@ -1,4 +1,4 @@
-(async () => {
+(() => {
   const MIN_CHROME_VERSION = 116;
 
   const getChromiumMajorVersion = (): number | null => {
@@ -22,149 +22,186 @@
       majorVersion !== null && majorVersion >= MIN_CHROME_VERSION;
 
     if (!window.documentPictureInPicture || !isVersionSupported) {
-      alert(
-        `この機能は Chromium ${MIN_CHROME_VERSION} 以降で利用できます。ブラウザを最新版へ更新してください。`
-      );
       return false;
     }
 
     return true;
   };
 
-  const styleSheet = `
-  body {
-    margin: 0;
-    background-color: black;
-  }
+  const startHuluPiP = async () => {
+    const styleSheet = `
+    body {
+      margin: 0;
+      background-color: black;
+    }
 
-  .strp-ads, .strp-ad-module, .vjs-text-track-display, .vjs-spinner, .vjs-error-display, .vjs-modal-dialog, .vjs-hidden {
-    display: none;
-  }
+    .strp-ads, .strp-ad-module, .vjs-text-track-display, .vjs-spinner, .vjs-error-display, .vjs-modal-dialog, .vjs-hidden {
+      display: none;
+    }
 
-  video {
-    width: 100%;
-    height: 100dvh;
-  }
-  
-  .hulu-pip-controls {
-    position: absolute;
-    bottom: 5dvh;
-    left: 0;
-    right: 0;
-    display: none;
-    gap: 20px;
-    width: fit-content;
-    margin: 0 auto;
-    padding: .8em 1.5em;
-    background-color: rgba(10, 10, 10, 0.85);
-    border: solid 1px rgba(50, 50, 50, 0.95);
-    border-radius: 100dvh;
-    backdrop-filter: blur(5px);
-  }
+    video {
+      width: 100%;
+      height: 100dvh;
+    }
+    
+    .hulu-pip-controls {
+      position: absolute;
+      bottom: 5dvh;
+      left: 0;
+      right: 0;
+      display: none;
+      gap: 20px;
+      width: fit-content;
+      margin: 0 auto;
+      padding: .8em 1.5em;
+      background-color: rgba(10, 10, 10, 0.85);
+      border: solid 1px rgba(50, 50, 50, 0.95);
+      border-radius: 100dvh;
+      backdrop-filter: blur(5px);
+    }
 
-  .hulu-pip-controls img {
-    cursor: pointer;
-    width: 1.2em;
-    height: 1.2em;
-    opacity: 0.8;
-  }
-  `;
+    .hulu-pip-controls img {
+      cursor: pointer;
+      width: 1.2em;
+      height: 1.2em;
+      opacity: 0.8;
+    }
+    `;
 
-  const vjsElem = document.getElementById("vjs_video_3");
-  if (!(vjsElem instanceof HTMLElement)) {
-    return;
-  }
+    let pipWindow: Window | null = null;
 
-  const videoElem = vjsElem.querySelector("video");
-  if (!(videoElem instanceof HTMLVideoElement)) {
-    return;
-  }
+    const observTarget = document.querySelector(".content-main");
+    if (!(observTarget instanceof HTMLElement)) {
+      return;
+    }
 
-  const parentElem = document.querySelector(".video-core-container");
-  if (!(parentElem instanceof HTMLElement)) {
-    return;
-  }
+    let intervalId: number | null = null;
 
-  if (!ensureDocumentPiPSupported()) {
-    return;
-  }
+    const observer = new MutationObserver((mutations) => {
+      for (const m of mutations) {
+        if (m.type === "childList" && m.removedNodes.length > 0) {
+          pipWindow?.document.body.replaceChildren();
+        } else if (m.type === "childList" && m.addedNodes.length > 0) {
+          if (intervalId) {
+            clearInterval(intervalId);
+          }
+          let cnt = 0;
+          intervalId = setInterval(() => {
+            if (mountPiPContent() || cnt++ > 100) {
+              clearInterval(intervalId!);
+            }
+          }, 300);
+        }
+      }
+    });
 
-  const pElem = document.createElement("p");
-  pElem.textContent = "ピクチャー イン ピクチャーで再生しています";
-  pElem.style.margin = "0";
-  pElem.style.padding = "1em 2em";
+    observer.observe(observTarget, {
+      childList: true,
+    });
 
-  try {
-    const pipWindow = await window.documentPictureInPicture.requestWindow({
+    let vjsElem: HTMLElement | null = null;
+    let parentElem: HTMLElement | null = null;
+    let pElem: HTMLParagraphElement | null = null;
+
+    const mountPiPContent = () => {
+      if (!pipWindow) return false;
+
+      vjsElem = document.querySelector("[id^='vjs_video_']");
+      if (!(vjsElem instanceof HTMLElement)) {
+        return false;
+      }
+
+      const videoElem = vjsElem.querySelector("video");
+      if (!(videoElem instanceof HTMLVideoElement) || videoElem.src === "") {
+        return false;
+      }
+
+      parentElem = document.querySelector(".video-core-container");
+      if (!(parentElem instanceof HTMLElement)) {
+        return false;
+      }
+
+      pElem = document.createElement("p");
+      pElem.textContent = "ピクチャー イン ピクチャーで再生しています";
+      pElem.style.margin = "0";
+      pElem.style.padding = "1em 2em";
+
+      const controlsElem = pipWindow.document.createElement("div");
+      controlsElem.classList.add("hulu-pip-controls");
+      pipWindow.document.body.addEventListener("mouseenter", () => {
+        controlsElem.style.display = "flex";
+      });
+      pipWindow.document.body.addEventListener("mouseleave", () => {
+        controlsElem.style.display = "none";
+      });
+
+      const playIconURL = chrome.runtime.getURL("img/play.png");
+      const pauseIconURL = chrome.runtime.getURL("img/pause.png");
+      const replay10IconURL = chrome.runtime.getURL("img/replay10.png");
+      const forward10IconURL = chrome.runtime.getURL("img/forward10.png");
+
+      const replay10ButtonElem = pipWindow.document.createElement("img");
+      replay10ButtonElem.src = replay10IconURL;
+      replay10ButtonElem.addEventListener("click", () => {
+        videoElem.currentTime -= 10;
+      });
+
+      const forward10ButtonElem = pipWindow.document.createElement("img");
+      forward10ButtonElem.src = forward10IconURL;
+      forward10ButtonElem.addEventListener("click", () => {
+        videoElem.currentTime += 10;
+      });
+
+      const playButtonElem = pipWindow.document.createElement("img");
+      playButtonElem.src = videoElem.paused ? playIconURL : pauseIconURL;
+      playButtonElem.addEventListener("click", () => {
+        if (videoElem.paused) {
+          videoElem.play();
+        } else {
+          videoElem.pause();
+        }
+      });
+      videoElem.addEventListener("play", () => {
+        playButtonElem.src = pauseIconURL;
+      });
+      videoElem.addEventListener("pause", () => {
+        playButtonElem.src = playIconURL;
+      });
+
+      controlsElem.append(replay10ButtonElem);
+      controlsElem.append(playButtonElem);
+      controlsElem.append(forward10ButtonElem);
+
+      pipWindow.document.body.append(vjsElem);
+      pipWindow.document.body.append(controlsElem);
+      
+      parentElem?.append(pElem);
+
+      return true;
+    }
+
+    pipWindow = await window.documentPictureInPicture.requestWindow({
       width: 640,
       height: 360,
     });
 
-    pipWindow.addEventListener("resize", () => {
-      // ToDo: 自動アスペクト比調整
-    });
-
+    
     const styleElem = pipWindow.document.createElement("style");
     styleElem.textContent = styleSheet;
+    pipWindow.document.head.append(styleElem);
 
-    const controlsElem = pipWindow.document.createElement("div");
-    controlsElem.classList.add("hulu-pip-controls");
-    pipWindow.document.body.addEventListener("mouseenter", () => {
-      controlsElem.style.display = "flex";
-    });
-    pipWindow.document.body.addEventListener("mouseleave", () => {
-      controlsElem.style.display = "none";
-    });
-
-    const playIconURL = chrome.runtime.getURL("img/play.png");
-    const pauseIconURL = chrome.runtime.getURL("img/pause.png");
-    const replay10IconURL = chrome.runtime.getURL("img/replay10.png");
-    const forward10IconURL = chrome.runtime.getURL("img/forward10.png");
-
-    const replay10ButtonElem = pipWindow.document.createElement("img");
-    replay10ButtonElem.src = replay10IconURL;
-    replay10ButtonElem.addEventListener("click", () => {
-      videoElem.currentTime -= 10;
-    });
-
-    const forward10ButtonElem = pipWindow.document.createElement("img");
-    forward10ButtonElem.src = forward10IconURL;
-    forward10ButtonElem.addEventListener("click", () => {
-      videoElem.currentTime += 10;
-    });
-
-    const playButtonElem = pipWindow.document.createElement("img");
-    playButtonElem.src = videoElem.paused ? playIconURL : pauseIconURL;
-    playButtonElem.addEventListener("click", () => {
-      if (videoElem.paused) {
-        void videoElem.play();
-      } else {
-        videoElem.pause();
-      }
-    });
-    videoElem.addEventListener("play", () => {
-      playButtonElem.src = pauseIconURL;
-    });
-    videoElem.addEventListener("pause", () => {
-      playButtonElem.src = playIconURL;
-    });
-
-    controlsElem.append(replay10ButtonElem);
-    controlsElem.append(playButtonElem);
-    controlsElem.append(forward10ButtonElem);
-
-    pipWindow.document.body.append(styleElem);
-    pipWindow.document.body.append(vjsElem);
-    pipWindow.document.body.append(controlsElem);
-
-    parentElem.append(pElem);
+    mountPiPContent();
 
     pipWindow.document.addEventListener("visibilitychange", () => {
-      pElem.remove();
-      styleElem.remove();
-      parentElem.append(vjsElem);
+      pElem?.remove();
+      parentElem?.append(vjsElem!);
+      observer.disconnect();
     });
-  } catch (error) {
-    console.error("PiPウィンドウの作成に失敗しました", error);
+  };
+
+  if (ensureDocumentPiPSupported()) {
+    startHuluPiP();
+  } else {
+    alert(`この機能は Chromium ${MIN_CHROME_VERSION} 以降で利用できます。ブラウザを最新版へ更新してください。`);
   }
 })();
